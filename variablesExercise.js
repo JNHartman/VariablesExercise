e64689316e578485e1de5422b89af2f61187765f
Jesse Hartman <jessenhartman@gmail.com>
2021-02-23 14:17:09 -0600

    corrected a few misteaks

--- variablesExercise.js
+++ variablesExercise.js
@@ -3,29 +3,36 @@
 
 // 2. Create a variable with the type of "let" and a name of "num". Set the value of the variable to the number 20.
 let num = 20;
+console.log(num);
 
 // 3. On the next line assign a new value of the number 15 for the num variable. (NOTE: Do not redeclare the variable just update/reassign the value for it.)
 num = 15;
+console.log(num);
 
 // 4. On the next line assign a new value for the num variable again by adding 7 to it (Must use the plus "+" sign when completing this line. HINT: There are two ways it can be written.)
-num = num + 7;
+num += 7;
+// num = num + 7;
+console.log(num);
 
 // 5. On the next line assign a new value for the num variable again by subtracting 1 from it. Also, must complete this without using an equal sign "=".
-num = num -1;
+num--;
+console.log(num);
 
 // 6. On the next line assign a new value for the num variable again by setting it to the following string "This is not a number"
-
+num = "This is not a number";
 console.log(num);
+
 // 7. Create a new variable with the type of "let" and a name of "blank", but do not set a value for it.
 let blank;
 
 // 8. Create a console log to display the value of the "blank" variable in terminal
-// console.log(blank);
+console.log(blank);
 
 // 9. On the next line assign a value for the blank variable to the value that represents nothing (NOTE: Assigning the value to the string "nothing" is not the correct answer.)
-// const blank = null;  
+blank = null;
+console.log(blank); 
 
-console.log(blank);
+
 
 //  BONUS
 
@@ -33,11 +40,11 @@ console.log(blank);
 const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
 
 // 2. On the next line create a console log that displays all four variable values (0.25, 0.10, etc.) on ONE line in terminal when the file is run.
-console.log(quarter + dime + nickel + penny);
+console.log(quarter, dime, nickel, penny);
 
 // 3. Create a new variable with the type of "let" and a name of "letters". Set the value of the variable to the following string "qwertyuiop".
-let letters = ("qwertyuiop")
-console.log(letters);
+let letters = "qwertyuiop";
 
 // 4. Using index and a console log, have the "p" at the end of the "letters" string variable display in the terminal when you run the file. Also, you must use the length property when selecting the "p".
-console.log(letters[9]);
+// console.log(letters[9]);
+console.log(letters[letters.length - 1]);
